615dbe6e8c4dada88716f1de69c89c68049b8df9
dilmurodxudoyberdiyev <dxudoyberdiyev041@gmail.com>
2022-11-17 20:29:32 +0500

    Lesson2

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let son = +prompt("pulingizni kiriting");
+
+let borish = 500;
+let mehmonxona = 250;
+let muziy = 120;
+let dollor = 10650.34;
+let yevro = 10650.03;
+
+let sum = (borish + mehmonxona) * dollor + (muziy * yevro);
+
+
+console.log(sum);
+
+let number = son - sum;
+
+console.log(number);
